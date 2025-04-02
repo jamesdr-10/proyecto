69a7f8afdff4112db7a9e10670d7c6dc89617056
JP-Guillen <juan_guillen@ucne.edu.do>
2025-04-01 21:29:21 -0400

    Borron

---
+++ BorronEstructuraFinal/BorronEstructuraFinal/AgregarUsuarios.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BorronEstructuraFinal;
+
+class Nodo
+{
+    public string Nombre;
+    public string Correo;
+    public string Telefono;
+    public int Edad;
+    public Nodo Siguiente;
+    public Nodo Anterior;
+
+
+    public Nodo(string nombre, string correo, string telefono, int edad)
+    {
+        Nombre = nombre;
+        Correo = correo;
+        Telefono = telefono;
+        Edad = edad;
+        Siguiente = null;
+        Anterior = null;
+    }
+}
+
+class ListaCircular
+{
+    Nodo Cabeza = null;
+    Nodo Cola = null;
+    Nodo Actual = null;
+    public void AgregarPersona()
+    {
+        Console.WriteLine("Introduzca el Nombre y apellido, CorreoElectronico, Telefono y edad:");
+        string nombre = Console.ReadLine();
+        string correo = Console.ReadLine();
+        string telefono = Console.ReadLine();
+        int edad = int.Parse(Console.ReadLine());
+
+        Nodo NuevoNodo = new Nodo(nombre, correo, telefono, edad);
+
+        if (Cabeza == null)
+        {
+            Cabeza = Cola = NuevoNodo;
+            Cabeza.Siguiente = Cabeza;
+            Cabeza.Anterior = Cabeza;
+        }
+        else
+        {   
+            NuevoNodo.Siguiente = Cabeza;
+            NuevoNodo.Anterior = Cola;
+            Cola.Siguiente = NuevoNodo;
+            Cabeza.Anterior = NuevoNodo;
+            Cola = NuevoNodo;
+        }
+    }
+
+   
+    //public void Imprimir()
+    //{
+    //    Actual = Cabeza;
+    //    if (Actual == null)
+    //    {
+    //        Console.WriteLine("Agregue un Usuario");
+    //        return;
+    //    }
+
+    //    do
+    //    {
+    //        Console.WriteLine($"Nombre: {Actual.Nombre}, Correo: {Actual.Correo}, Teléfono: {Actual.Telefono}, Edad: {Actual.Edad}");
+    //        Actual = Actual.Siguiente;
+    //    } while (Actual != Cabeza);
+    //}
+
+   
+
+
+}
+
+
---
+++ BorronEstructuraFinal/BorronEstructuraFinal/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using BorronEstructuraFinal;
+namespace BorronFinal;
+
+
+class program
+{
+
+    public static void MENU()
+    {
+        Console.WriteLine("**********Bienvenido********** ");
+        Console.WriteLine("1 - Agregar Nuevo usuario ");
+        Console.WriteLine("2 - Agregar Amigos ");
+        Console.WriteLine("3 - Ver Solicitudes enviadas ");
+        Console.WriteLine("4 - ver amigos ");
+        Console.WriteLine("5 - Ver personas que te siguen");
+        Console.WriteLine("6 - Ver solicitudes de Amistad ");
+        Console.WriteLine("7 - Ver Arbol de la persona");
+        Console.WriteLine("8 - Ver listado de personas en orden ascendente.  ");
+        Console.WriteLine("9 - Ver listado de personas en orden descendente.");
+        Console.WriteLine("10 - Ver factor de carga del directorio telefónico. ");
+        Console.WriteLine("11 - Cambiar Usuario. ");
+        Console.WriteLine("0 - Salir ");
+        Console.WriteLine("****Digite la opcion que desea****");
+    }
+
+    static void Main(string[] args)
+    { 
+        ListaCircular lista = new ListaCircular();
+        
+        int numero;
+        do
+        {
+            Console.Clear();
+            lista.Recorrer();
+            MENU();
+
+            numero = int.Parse(Console.ReadLine());
+            if (numero > 11 || numero < 0)
+            {
+                Console.Clear();
+            }
+
+            switch (numero)
+            {
+                case 1:
+                    lista.AgregarPersona();
+                    Console.Clear();
+
+                    break;
+                case 2:
+                    Console.WriteLine("EN MANTENIMIENTO");
+
+                    break;
+                case 3:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 4:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 5:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 6:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 7:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 8:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 9:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 10:
+                    Console.WriteLine("EN MANTENIMIENTO");
+                    break;
+                case 11:
+                    Console.Clear();
+                    lista.Recorrer();
+                    break;
+            }
+        } while (numero != 0);
+    }
+
+}
