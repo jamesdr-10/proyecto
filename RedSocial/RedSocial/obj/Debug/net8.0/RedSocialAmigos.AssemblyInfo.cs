30c4a815241c3f8a1aa9634b52ced816343e68c6
jamesdr-10 <james_depena@ucne.edu.do>
2025-04-07 23:16:54 -0400

    Proyecto

--- RedSocial/RedSocial/obj/Debug/net8.0/RedSocialAmigos.AssemblyInfo.cs
+++ RedSocial/RedSocial/obj/Debug/net8.0/RedSocialAmigos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RedSocialAmigos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682856cece195faaa8567d2a2704d883a8f1dc07")]
 [assembly: System.Reflection.AssemblyProductAttribute("RedSocialAmigos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RedSocialAmigos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
